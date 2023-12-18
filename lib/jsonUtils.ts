af98b1c4034e508b0b994da1ac724d9eb905c4be
rohithgilla <gillarohith1@gmail.com>
2023-12-18 18:31:12 +0530

    Make Json api -> Json possible

---
+++ lib/jsonUtils.ts
@@ -0,0 +1,25 @@
+import { deattribute, deserialise, linkRelationships } from "kitsu-core"
+
+export const jsonApiToJs = (
+  input: any
+): Record<string, any> | Record<string, any>[] => {
+  try {
+    const jsonApi = JSON.parse(input)
+
+    const output = linkRelationships(jsonApi.data, jsonApi.included)
+    const normalized = deattribute(output)
+
+    for (const key in Object.keys(normalized)) {
+      if (!!normalized[key]?.data) {
+        normalized[key] = normalized[key].data
+      }
+    }
+
+    normalized.meta = jsonApi.meta
+
+    return normalized
+  } catch (e) {
+    console.log(e)
+    return {}
+  }
+}
